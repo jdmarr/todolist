cdcc2adfa9e31a7085db94518ae3070491b8b944
Jordan Marr <jmarr.to@gmail.com>
2021-01-27 21:25:04 -0800

    Remove about route

--- app.js
+++ app.js
@@ -160,10 +160,6 @@ app.get("/list/:customListName", function(req, res) {
   }
 });
 
-app.get("/about", function(req, res) {
-  res.render("about");
-});
-
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
